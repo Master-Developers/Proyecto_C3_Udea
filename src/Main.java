c308f93f9627c04e7f2538253dcedb4b7c580897
Mateo8a <mateo.rodriguez@uptc.edu.co>
2022-08-18 22:14:40 -0500

    "34"

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Prueba de acceso al repositorio");
-        System.out.printf("Prueba 2");
-        System.out.println("Prueba3");
-        
-
+        System.out.println("Prueba 2");
+        System.out.println("Prueba34");
 
     }
 }
