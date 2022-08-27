5a33ebac83e434d52b68a4abb9145d19ae2cbc83
GERMAN8901 <106921625+GERMAN8901@users.noreply.github.com>
2022-08-26 22:55:41 -0500

    se crea atributos a clase empleado

--- src/modelo/Empleado.java
+++ src/modelo/Empleado.java
@@ -1,18 +1,25 @@
 package modelo;
 
 public class Empleado {
-        private String Nombre;
-        private String Correo;
-        private String Empresa;
-        private String rol; //Administratico operativo
+    // Atributos
+    private String Nombre;
+    private int Cedula;
+    private int Numero_Celular;
+    private String Correo;
+    private String Empresa;
+    private String rol; // Administratico operativo
+    // Constructor
 
-    public Empleado(String nombre, String correo, String empresa, String rol) {
+    public Empleado(String nombre, int cedula, int numero_Celular, String correo, String empresa) {
         Nombre = nombre;
+        Cedula = cedula;
+        Numero_Celular = numero_Celular;
         Correo = correo;
         Empresa = empresa;
         this.rol = rol;
     }
 
+    // Getter y Setter
     public String getNombre() {
         return Nombre;
     }
@@ -43,5 +50,21 @@ public class Empleado {
 
     public void setRol(String rol) {
         this.rol = rol;
+    }
+
+    public int getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(int cedula) {
+        Cedula = cedula;
+    }
+
+    public int getNumero_Celular() {
+        return Numero_Celular;
+    }
+
+    public void setNumero_Celular(int numero_Celular) {
+        Numero_Celular = numero_Celular;
     }
 }
