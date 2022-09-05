165eea7f95789bb226568e34261e41d1cf00429e
GERMAN8901 <106921625+GERMAN8901@users.noreply.github.com>
2022-09-04 21:17:17 -0500

    creando el sprindbood para el codigo

---
+++ src/modelo/proyecto/src/test/java/com/proyecto/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
